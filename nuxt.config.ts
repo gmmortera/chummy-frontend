0ee16576f6729c4fb359a6d476e1ce8946a44532
Gianfranco <gfmmortera@gmail.com>
2025-04-06 14:41:34 +0800

    Add responsive design

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: 'Chummy'
-    }
-  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   colorMode: 'system',
